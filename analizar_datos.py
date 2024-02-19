997dfee3cdeee08e8d98b1b21b4a9f2b8900b82b
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-19 10:00:46 +0100

    analizamos datos

--- analizar_datos.py
+++ analizar_datos.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.stat import Correlation
 
 # Crear una sesión de Spark
 spark = SparkSession.builder.appName("AnalizarDatos").getOrCreate()
@@ -12,6 +14,16 @@ df = df.dropna()
 #eliminamos datos duplicados 
 df = df.dropDuplicates()
 
+#Analizamos los datos
+print("Número de filas: ", df.count())
+print("Número de columnas: ", len(df.columns))
+df.describe().show()  # Resumen estadístico de los datos
+
+#matriz de correlación de las variables numéricas
+assembler = VectorAssembler(inputCols=df.columns, outputCol="features")
+datos_transformados = assembler.transform(df).select("features")
+matriz_correlacion = Correlation.corr(datos_transformados, "features").head()
+print("Matriz de correlación: \n", matriz_correlacion[0])
 
 #Cerramos la sesión de Spark
 spark.stop()
