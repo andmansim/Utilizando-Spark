f3d4d42a6a35113f40d24528e40c155c8bebd65b
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-19 10:04:49 +0100

    okey

--- analizar_datos.py
+++ analizar_datos.py
@@ -1,6 +1,8 @@
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.stat import Correlation
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Crear una sesión de Spark
 spark = SparkSession.builder.appName("AnalizarDatos").getOrCreate()
@@ -23,7 +25,13 @@ df.describe().show()  # Resumen estadístico de los datos
 assembler = VectorAssembler(inputCols=df.columns, outputCol="features")
 datos_transformados = assembler.transform(df).select("features")
 matriz_correlacion = Correlation.corr(datos_transformados, "features").head()
-print("Matriz de correlación: \n", matriz_correlacion[0])
+array_correlacion = matriz_correlacion[0].toArray()
+
+#imprimimos la matriz de correlación
+print("Matriz de correlación: ")
+sns.heatmap(array_correlacion, annot=True, fmt=".2f")
+plt.title('Matriz de correlación de las variables numéricas')
+plt.show()
 
 #Cerramos la sesión de Spark
 spark.stop()
